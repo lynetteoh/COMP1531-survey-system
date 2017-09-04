85656fba329e821df00c5d3801b3bfd8355e09a9
IanThorvaldson <z5161938@unsw.edu.au>
2017-09-04 13:07:20 +1000

    Added message when the username was entered incorrectly

--- login.py
+++ login.py
@@ -4,7 +4,8 @@ from security import login_success
 
 def login_page(request, page):
 	if request.method == "GET":
-		return render_template("login.html", page = page)
+		print(request.args.get('attempt'))
+		return render_template("login.html", page = page, attempt = request.args.get('attempt'))
 	if request.method == "POST":
 		success = login_success(request.form["username"], request.form["password"], request.remote_addr)
 		if success == "Success":
@@ -12,6 +13,9 @@ def login_page(request, page):
 				return redirect(page.replace("@2F", "/"))
 			return redirect("/home")
 		else:
+			attempt = 1
+			if request.args.get('attempt'):
+				attempt = int(request.args.get('attempt')) + 1
 			if page:
-				return redirect("/login/"+page)
-			return redirect("/login")
+				return redirect("/login/"+page + "?attempt=" + str(attempt))
+			return redirect("/login"+ "?attempt=" + str(attempt))
