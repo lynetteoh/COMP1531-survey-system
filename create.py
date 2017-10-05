ff64152add7ac4e6890d4789ecdcf6270615a5de
IanThorvaldson <z5161938@unsw.edu.au>
2017-10-05 17:15:37 +1100

    Ensured surveys can only be created for courses with no survey yet created

--- create.py
+++ create.py
@@ -2,17 +2,14 @@
 
 from flask import Flask, request, render_template
 import csv
+from courses import COURSES
 
 COURSE_LISTING = 'courses.csv'
 
-def view_courses(request, active_surveys):
-	courses = []
-	with open(COURSE_LISTING,'r') as csv_in:
-		reader = csv.reader(csv_in)
-		first_line_read = False
-		for row in reader:
-			if len(row) == 1:
-				row = row[0].split(" ")
-			if (row[0], row[1]) not in active_surveys:
-				courses.append(row)
-	return render_template("select_course.html", courses = courses)
+def view_courses(request, existing_surveys):
+	surveyed_courses = [x.course for x in existing_surveys]
+	unsurveyed_courses = []
+	for course in COURSES:
+		if course not in surveyed_courses:
+			unsurveyed_courses.append(course)
+	return render_template("select_course.html", courses = unsurveyed_courses)
